c9cf25d881f76ec3501e17c2900d11ae88563803
ViktorChernykh <v-cher9@yandex.ru>
2022-12-15 17:07:51 +0300

    Change DesireToggleDto

--- Sources/ProductDto/Request/Desire.swift
+++ Sources/ProductDto/Request/Desire.swift
@@ -7,24 +7,6 @@
 
 import Foundation
 
-public struct DesireListDto: Codable {
-	// MARK: - Stored properties
-	public let lang: String?
-	public let page: Int?
-	public let per: Int?
-
-	// MARK: - Init
-	public init(
-		lang: String?,
-		page: Int?,
-		per: Int?
-	) {
-		self.lang = lang
-		self.page = page
-		self.per = per
-	}
-}
-
 public struct DesireToggleDto: Codable {
 	// MARK: - Stored properties
 	public let productIds: [UUID]
