309b17ee7713f7c01bc39d2e09dd2f9910e0fa60
Viсtor Chernykh <v-cher9@yandex.ru>
2023-01-17 18:50:43 +0300

    Change

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version: 5.7
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "product-dto",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v13),
+        .macOS(.v10_15),
+        .iOS(.v10),
     ],
     products: [
         .library(name: "ProductDto", targets: ["ProductDto"]),
